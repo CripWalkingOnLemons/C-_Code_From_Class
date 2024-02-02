c1d31049bd5660a3e5876ae8edcb110c86e69d02
Ethan <113372967+CripWalkingOnLemons@users.noreply.github.com>
2024-02-02 13:12:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int stringlen = rand.Next(8, 8);
+            int randValue;
+            string str = "";
+            char letter;
+            for (int i = 0; i < stringlen; i++)
+            {
+
+                
+                randValue = rand.Next(0, 26);
+
+                
+                letter = Convert.ToChar(randValue + 65);
+
+                 
+                str = str + letter;
+            }
+            Console.WriteLine("Generated password:" + str);
+        }
+    }
+}
